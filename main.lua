5849046480f404f8d877b347a8fa7c0ae8aef5e9
iSkidYourScripts <121834227+iSkidYourScripts@users.noreply.github.com>
2023-01-03 12:12:31 -0500

    Create main.lua

---
+++ main.lua
@@ -0,0 +1,5 @@
+if game.PlaceId == 155615604 then
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/iSkidYourScripts/Ghost-Hub/main/main/prisionlife.lua",true))()
+elseif game.PlaceId == 7991339063 then
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/iSkidYourScripts/Ghost-Hub/main/main/rainbowfriends.lua",true))()
+end
